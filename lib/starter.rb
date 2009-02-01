ef7b72364e9d4defd2290edd5acd1138542279ac
Harald Sitter <apachelogger@ubuntu.com>
2009-02-01 12:58:32 +0100

    Only invoke custom if the method actually exists. This cuts the overall requirements for a release script to 4 SLOC (3 constants + starter require)

--- lib/starter.rb
+++ lib/starter.rb
@@ -40,6 +40,8 @@ fetch_doc unless $options[:doc] == false
 
 create_tag if $options[:tag] == true
 
-custom unless $options[:app] == false
+custom unless $options[:app] == false or not self.respond_to?("custom")
 
 create_tar unless $options[:tar] == false
+
+create_packager_notification unless $options[:pkgnotify] == false
