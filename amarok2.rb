6d478b91458db0fa0ff42bc972ae85451b150d6c
Harald Sitter <apachelogger@ubuntu.com>
2009-09-01 20:28:48 +0200

    Also nuke playground and docs from amarok tar

--- amarok2.rb
+++ amarok2.rb
@@ -44,7 +44,7 @@ def custom
 
     remover([
         "Amarok.kdev4","release_scripts","supplementary_scripts","HACKING",
-        "VIS_PLAN",".krazy","bamboo"
+        "VIS_PLAN",".krazy","bamboo","playground","docs"
     ])
     base_dir
 
