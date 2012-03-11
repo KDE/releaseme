ae1a1e6c5a6c3942c101ebf273eea7d4fe755967
Jean-Baptiste Mardelle <jb@kdenlive.org>
2012-03-11 22:00:23 +0100

    Initial Kdenlive releaseme script

---
+++ kdenlive.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+NAME      = "kdenlive"
+COMPONENT = "extragear"
+SECTION   = "multimedia"
+
+$srcvcs   = "git"
+
+def custom
+end
+
+# get things started
+require 'lib/starter'
