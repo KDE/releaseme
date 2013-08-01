870aea72cc517a1762ecacbfaa77757e4835963e
Harald Sitter <sitter@kde.org>
2013-08-01 14:06:26 +0200

    kscreen is currently in kdereview

--- kscreen.rb
+++ kscreen.rb
@@ -19,8 +19,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 NAME      = "kscreen"
-COMPONENT = "playground"
-SECTION   = "base"
+COMPONENT = "kdereview"
+SECTION   = ""
 
 $: << File.dirname( __FILE__)
 $srcvcs   = "git"
