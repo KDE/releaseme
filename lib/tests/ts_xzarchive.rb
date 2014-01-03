c7dc458d8317623591b25655082c431ed91fb4a2
Harald Sitter <sitter@kde.org>
2014-01-03 13:02:57 +0100

    split xzarchive test_create into multiple tests

--- lib/tests/ts_xzarchive.rb
+++ lib/tests/ts_xzarchive.rb
@@ -24,7 +24,7 @@ class TestXzArchive < Test::Unit::TestCase
         return tarFile() + ".xz"
     end
 
-    def test_directory
+    def test_attr_directory
         a = XzArchive.new()
         assert_equal(a.directory, nil)
 
@@ -32,7 +32,7 @@ class TestXzArchive < Test::Unit::TestCase
         assert_equal(a.directory, @dir)
     end
 
-    def test_rate
+    def test_attr_level
         a = XzArchive.new()
         a.directory = @dir
         assert_equal(a.level, 9)
@@ -41,40 +41,44 @@ class TestXzArchive < Test::Unit::TestCase
         assert_equal(a.level, 5)
     end
 
+    # Proper setup.
+    # Must return true.
+    # Must have @dir.tar.xz.
+    # Must not have @dir.tar.
     def test_create_valid
         a = XzArchive.new()
         a.directory = @dir
 
-        # Proper setup.
-        # Must have returned with true.
-        # Must have @dir.tar.xz.
-        # Must not have @dir.tar.
-        FileUtils.rm_rf(tarFile())
-        FileUtils.rm_rf(xzFile())
         a.level = 1
         ret = a.create()
         assert_equal(ret, true)
         assert(File::exists?(xzFile()))
         assert(!File::exists?(tarFile()))
+    end
 
-        # Bogus compression level.
-        # Must return with false.
-        # Must not have @dir.tar.
-        # Must not have @dir.tar.xz
-        FileUtils.rm_rf(tarFile())
-        FileUtils.rm_rf(xzFile())
+    # Bogus compression level.
+    # Must return false.
+    # Must not have @dir.tar.
+    # Must not have @dir.tar.xz
+    def test_create_invalid_level
+        a = XzArchive.new()
+        a.directory = @dir
+
         a.level = -1
         ret = a.create()
         assert_equal(ret, false)
         assert(!File::exists?(tarFile()))
         assert(!File::exists?(xzFile()))
+    end
 
-        # Directory does not exist.
-        # Must return with false
-        # Must not have @dir.tar.
-        # Must not have @dir.tar.xz
-        FileUtils.rm_rf(tarFile())
-        FileUtils.rm_rf(xzFile())
+    # Directory does not exist.
+    # Must return false.
+    # Must not have @dir.tar.
+    # Must not have @dir.tar.xz
+    def test_create_invalid_dir
+        a = XzArchive.new()
+        a.directory = @dir
+
         d = "tmp_xz_" + (0...16).map{ ('a'..'z').to_a[rand(26)] }.join
         a.directory = d
         ret = a.create()
