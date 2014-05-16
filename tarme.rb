9105c15e04657f51eb5c31a0fdeac5fad6a68baa
Harald Sitter <sitter@kde.org>
2014-05-16 10:47:51 +0200

    fix single project releases

--- tarme.rb
+++ tarme.rb
@@ -1,4 +1,23 @@
 #!/usr/bin/env ruby
+#--
+# Copyright (C) 2014 Harald Sitter <apachelogger@ubuntu.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy
+# defined in Section 14 of version 3 of the license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
 
 require 'optparse'
 
@@ -78,7 +97,7 @@ def flat_project_resolver(project_id)
         end
     else
         # Project release -> resolve through REXML query to project level
-        project_element = find_element_from_xpath('/kdeprojects/component/module/project', project.id)
+        project_element = find_element_from_xpath('/kdeprojects/component/module/project', project_id)
         p = Project.new(project_element.attribute('identifier').to_s)
         p.resolve!
         release_projects << p
