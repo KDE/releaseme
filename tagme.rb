cecf950172342db8b6942485a9381a836bdf84c8
Harald Sitter <sitter@kde.org>
2014-06-11 12:06:21 +0200

    add tagme, working on release_data from tarme
    noop right now, only prints git commands

---
+++ tagme.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/env ruby
+#--
+# Copyright (C) 2014 Harald Sitter <apachelogger@ubuntu.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy
+# defined in Section 14 of version 3 of the license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: tarme.rb [options] PROJECT_NAME"
+
+    opts.on("--origin trunk|stable", [:trunk, :stable],
+            "Origin.",
+            "   Used to deduce release branch and localization branches.") do |v|
+        options[:origin] = v
+    end
+
+    opts.on("--version VERSION",
+            "Version.",
+            "   Versions should be kept in purely numerical format (good: x.x.x).",
+            "   Alphanumerical version should be avoided if at all possible (bad: x.x.xbeta1).") do |v|
+        options[:version] = v
+    end
+end.parse!
+
+if options[:origin].nil? or options[:version].nil? or ARGV.empty?
+    puts "error, you need to set origin and version"
+    exit 1
+end
+
+# TODO: single project tag
+# project_name = ARGV.pop
+#
+# p options
+# p ARGV
+# p project_name
+
+#################
+
+require_relative 'lib/project'
+require_relative 'lib/source'
+
+class TagProject
+    attr :project, true
+    attr :git_rev, true
+
+end
+
+def read_release_data()
+    projects = Array.new
+    File.open('release_data', 'r') do | file |
+        file.each_line do | line |
+            parts = line.split(';')
+            next if parts.size < 3 # If we don't manage 3 parts the line is definitely crap.
+            # 0 = project
+            # 1 = branch
+            # 2 = git rev
+            project = TagProject.new
+            project.project = Project.new(parts[0])
+            project.project.resolve!
+            project.project.vcs.branch = parts[1]
+            project.git_rev = parts[2]
+            projects << project
+        end
+    end
+    return projects
+end
+
+tag_projects = read_release_data()
+tag_projects.each do | tag_project |
+    puts "--- #{tag_project.project.id} ---"
+    source = Source.new
+    source.target = "tmp-tagme"
+    source.cleanup()
+    source.get(tag_project.project.vcs)
+
+    Dir.chdir(source.target) do
+        puts "::git tag #{options[:version]} #{tag_project.git_rev}"
+#         %x[git tag #{options[:version]} #{tag_project.git_rev}]
+        puts "::git push origin #{tag_project.project.vcs.branch}"
+#         %x[git push origin #{tag_project.project.vcs.branch}]
+    end
+
+    # TODO: impl l10n and docs and what have you
+end
