8e4dd30c65721c4973079c73b8a4d540d3a32466
Jan Grulich <jgrulich@redhat.com>
2014-06-19 14:20:38 +0200

    Update plasma-nm config  files

--- plasma-nm.rb
+++ plasma-nm.rb
@@ -19,8 +19,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 NAME      = "plasma-nm"
-COMPONENT = "playground"
-SECTION   = "network"
+COMPONENT = "extragear"
+SECTION   = "base"
 
 $: << File.dirname( __FILE__)
 $srcvcs   = "git"
