d90f87acf7d5de1e9264197afbaa1b46e48a3a0c
Jonathan Riddell <jr@jriddell.org>
2014-07-10 20:16:44 +0200

    updates for plasma 5.0

--- lib/kdel10n.rb
+++ lib/kdel10n.rb
@@ -119,7 +119,8 @@ class KdeL10n < Source
     end
 
     def po_file_dir(lang)
-        return "#{lang}/messages/#{@module}-#{@section}"
+        #return "#{lang}/messages/#{@module}-#{@section}"
+        return "#{lang}/messages/kde-workspace"
     end
 
     def retry_cmd?(exit_code, thing)
