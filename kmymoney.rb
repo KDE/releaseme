147c58bf8592c1b9c6d1d59ffc4e60c27cf8b62b
Cristian Oneț <onet.cristian@gmail.com>
2014-10-01 08:04:31 +0300

    Fix the generated tarball, don't add the 'doc' directory twice.

--- kmymoney.rb
+++ kmymoney.rb
@@ -8,6 +8,16 @@ $: << File.dirname( __FILE__)
 $srcvcs   = "git"
 
 def custom
+    # Fix the CMakeLists as translated documentation is included in
+    # releases
+
+    file = File.new( "CMakeLists.txt", File::RDWR )
+    str = file.read
+    file.rewind
+    file.truncate( 0 )
+    str.sub!( /add_subdirectory\( doc \)/, "" )
+    file << str
+    file.close
 end
 
 require 'lib/starter'
