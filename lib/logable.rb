ab0477c37075e7db1467457d244526f1fc610dd5
Harald Sitter <sitter@kde.org>
2015-01-28 13:00:54 +0100

    logable: do not print datetime by default, way too verbose

--- lib/logable.rb
+++ lib/logable.rb
@@ -68,6 +68,7 @@ module Logable
     def create_logger
       @__logger = Logger.new(STDOUT)
       @__logger.level = Logger::INFO
+      @__logger.datetime_format = ''
       # Module classes are not useful, use the actual module name if we are
       # mixed into a module.
       if self.class == Module || self.class == Class
