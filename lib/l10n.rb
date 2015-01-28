40ed1884a94e74045a947c18348e4a7fd691b7de
Harald Sitter <sitter@kde.org>
2015-01-28 13:31:45 +0100

    l10n: rbeautify

--- lib/l10n.rb
+++ lib/l10n.rb
@@ -27,161 +27,161 @@ require_relative 'translationunit'
 
 # FIXME: doesn't write master cmake right now...
 class L10n < TranslationUnit
-    def find_templates(directory, pos=Array.new)
-        Dir.glob("#{directory}/**/**/Messages.sh").each do |file|
-            File.readlines(file).each do |line|
-                line.match(/[^\/]*\.pot/).to_a.each do |match|
-                    pos << match.sub(".pot",".po")
-                end
-            end
+  def find_templates(directory, pos = [])
+    Dir.glob("#{directory}/**/**/Messages.sh").each do |file|
+      File.readlines(file).each do |line|
+        line.match(/[^\/]*\.pot/).to_a.each do |match|
+          pos << match.sub(".pot",".po")
         end
-        # Templates must be unique as multiple lines can contribute to the same
-        # template, as such it can happen that a.pot appears twice which can
-        # have unintended consequences by an outside user of the Array.
-        return pos.uniq
+      end
+    end
+    # Templates must be unique as multiple lines can contribute to the same
+    # template, as such it can happen that a.pot appears twice which can
+    # have unintended consequences by an outside user of the Array.
+    pos.uniq
+  end
+
+  # FIXME: this has no test backing right now
+  def strip_comments(file)
+    # Strip #~ lines, which once were sensible translations, but then the
+    # strings got removed, so they now stick around in case the strings
+    # return, poor souls, waiting for a comeback, reminds me of Sunset Blvd :(
+    # Problem is that msgfmt adds those to the binary!
+    file = File.new(file, File::RDWR)
+    str = file.read
+    file.rewind
+    file.truncate(0)
+    # Sometimes a fuzzy marker can precede an obsolete translation block, so
+    # first remove any fuzzy obsoletion in the file and then remove any
+    # additional obsoleted lines.
+    # This prevents the fuzzy markers from getting left over.
+    str.gsub!(/^#, fuzzy\n#~.*/, '')
+    str.gsub!(/^#~.*/, '')
+    str = str.strip
+    file << str
+    file.close
+  end
+
+  def po_file_dir(lang)
+    "#{lang}/messages/#{@i18n_path}"
+  end
+
+  def retry_cmd?(exit_code, thing)
+    if exit_code != 0
+      return false
+      fail 'dialog not implemented'
+      #retry_ = $dlg.yesnocancel("Fetching of #{thing} failed.", "Retry", "Skip")
+      puts retry_
+      exit 1 if retry_.nil?
+      return retry_
+    end
+    return false
+  end
+
+  def get_single(lang)
+    tempDir = "l10n"
+    FileUtils.rm_rf(tempDir)
+    Dir.mkdir(tempDir)
+
+    # TODO: maybe class this
+    poFileName = templates[0]
+    vcsFilePath = "#{po_file_dir(lang)}/#{poFileName}"
+    poFilePath = "#{tempDir}/#{poFileName}"
+
+    gotInfo = false
+    begin
+      ret = vcs.export(poFilePath, vcsFilePath)
+      # If the export failed, try to see if there is a file, if this command also
+      # fails then we have to assume the file is not present in SVN.
+      # Of course it still might, but the connection could be busted, but that
+      # is a lot less likely to be the case for 2 independent commands.
+      if not gotInfo and not ret
+        # If the info also failed, declare the file as not existent and
+        # prevent a retry dialog annoyance.
+        break if not vcs.exist?(vcsFilePath)
+        gotInfo = true
+      end
+    end while retry_cmd?(ret, "#{vcsFilePath}")
+
+    files = Array.new
+    if File.exist?(poFilePath)
+      files << poFilePath
+      strip_comments(poFilePath)
+    end
+    return files.uniq
+  end
+
+  def get_multiple(lang)
+    tempDir = "l10n"
+    FileUtils.rm_rf(tempDir)
+    Dir.mkdir(tempDir)
+
+    vcsDirPath = po_file_dir(lang)
+
+    return Array.new if vcs.list(vcsDirPath).empty?
+    begin
+      vcs.get(tempDir, vcsDirPath)
+    end while retry_cmd?($?, vcsDirPath)
+
+    files = Array.new
+    templates.each do |po|
+      poFilePath = tempDir.dup.concat("/#{po}")
+      next if not File.exist?(poFilePath)
+      files << poFilePath
+      strip_comments(poFilePath)
     end
 
-    # FIXME: this has no test backing right now
-    def strip_comments(file)
-        # Strip #~ lines, which once were sensible translations, but then the
-        # strings got removed, so they now stick around in case the strings
-        # return, poor souls, waiting for a comeback, reminds me of Sunset Blvd :(
-        # Problem is that msgfmt adds those to the binary!
-        file = File.new(file, File::RDWR)
-        str = file.read
-        file.rewind
-        file.truncate(0)
-        # Sometimes a fuzzy marker can precede an obsolete translation block, so
-        # first remove any fuzzy obsoletion in the file and then remove any
-        # additional obsoleted lines.
-        # This prevents the fuzzy markers from getting left over.
-        str.gsub!(/^#, fuzzy\n#~.*/, '')
-        str.gsub!(/^#~.*/, '')
-        str = str.strip
-        file << str
-        file.close
-    end
+    files.uniq
+  end
 
-    def po_file_dir(lang)
-        return "#{lang}/messages/#{@i18n_path}"
-    end
+  def get(sourceDirectory)
+    target = sourceDirectory + "/po/"
+    Dir.mkdir(target)
 
-    def retry_cmd?(exit_code, thing)
-        if exit_code != 0
-            return false
-            raise "dialog not implemented"
-            #retry_ = $dlg.yesnocancel("Fetching of #{thing} failed.", "Retry", "Skip")
-            puts retry_
-            exit 1 if retry_.nil?
-            return retry_
+    availableLanguages = vcs.cat("subdirs").split("\n")
+    @templates = find_templates(sourceDirectory)
+
+    anyTranslations = false
+    Dir.chdir(sourceDirectory) do
+      availableLanguages.each do | language |
+        next if language == 'x-test'
+
+        puts "Downloading #{language} translations for #{sourceDirectory}"
+        if templates.count > 1
+          files = get_multiple(language)
+        elsif templates.count == 1
+          files = get_single(language)
+        else
+          # FIXME: needs testcase
+          return # No translations need fetching
         end
-        return false
-    end
 
-    def get_single(lang)
-        tempDir = "l10n"
-        FileUtils.rm_rf(tempDir)
-        Dir.mkdir(tempDir)
-
-        # TODO: maybe class this
-        poFileName = templates[0]
-        vcsFilePath = "#{po_file_dir(lang)}/#{poFileName}"
-        poFilePath = "#{tempDir}/#{poFileName}"
-
-        gotInfo = false
-        begin
-            ret = vcs.export(poFilePath, vcsFilePath)
-            # If the export failed, try to see if there is a file, if this command also
-            # fails then we have to assume the file is not present in SVN.
-            # Of course it still might, but the connection could be busted, but that
-            # is a lot less likely to be the case for 2 independent commands.
-            if not gotInfo and not ret
-                # If the info also failed, declare the file as not existent and
-                # prevent a retry dialog annoyance.
-                break if not vcs.exist?(vcsFilePath)
-                gotInfo = true
-            end
-        end while retry_cmd?(ret, "#{vcsFilePath}")
-
-        files = Array.new
-        if File.exist?(poFilePath)
-            files << poFilePath
-            strip_comments(poFilePath)
+        # No files obtained :(
+        if files.empty?
+          puts '  got no translations, skipping.'
+          next
         end
-        return files.uniq
+        anyTranslations = true
+
+        # TODO: path confusing with target
+        destinationDir = "po/" + language
+        Dir.mkdir(destinationDir)
+        FileUtils.mv(files, destinationDir)
+        #mv( ld + "/.svn", dest ) if $options[:tag] # Must be fatal iff tagging
+
+        # add to SVN in case we are tagging
+        #%x[svn add #{dest}/CMakeLists.txt] if $ptions[:tag]
+        @languages += [language]
+      end
+      # Make sure the temp dir is cleaned up
+      FileUtils::rm_rf('l10n')
+      if anyTranslations
+        # Update CMakeLists.txt
+        CMakeEditor::append_po_install_instructions!(Dir.pwd, 'po')
+      else
+        # Remove the empty translations directory
+        Dir.delete('po')
+      end
     end
-
-    def get_multiple(lang)
-        tempDir = "l10n"
-        FileUtils.rm_rf(tempDir)
-        Dir.mkdir(tempDir)
-
-        vcsDirPath = po_file_dir(lang)
-
-        return Array.new if vcs.list(vcsDirPath).empty?
-        begin
-            vcs.get(tempDir, vcsDirPath)
-        end while retry_cmd?($?, vcsDirPath)
-
-        files = Array.new
-        templates.each do |po|
-            poFilePath = tempDir.dup.concat("/#{po}")
-            next if not File.exist?(poFilePath)
-            files << poFilePath
-            strip_comments(poFilePath)
-        end
-        return files.uniq
-    end
-
-    def get(sourceDirectory)
-        target = sourceDirectory + "/po/"
-        Dir.mkdir(target)
-
-        availableLanguages = vcs.cat("subdirs").split("\n")
-        @templates = find_templates(sourceDirectory)
-
-        anyTranslations = false
-        Dir.chdir(sourceDirectory) do
-            availableLanguages.each do | language |
-                next if language == 'x-test'
-
-                puts "Downloading #{language} translations for #{sourceDirectory}"
-                if templates.count > 1
-                    files = get_multiple(language)
-                elsif templates.count == 1
-                    files = get_single(language)
-                else
-                    # FIXME: needs testcase
-                    return # No translations need fetching
-                end
-
-                # No files obtained :(
-                if files.empty?
-                    puts '  got no translations, skipping.'
-                    next
-                end
-                anyTranslations = true
-
-                # TODO: path confusing with target
-                destinationDir = "po/" + language
-                Dir.mkdir(destinationDir)
-                FileUtils.mv(files, destinationDir)
-                #mv( ld + "/.svn", dest ) if $options[:tag] # Must be fatal iff tagging
-
-                # add to SVN in case we are tagging
-                #%x[svn add #{dest}/CMakeLists.txt] if $ptions[:tag]
-                @languages += [language]
-            end
-            # Make sure the temp dir is cleaned up
-            FileUtils::rm_rf('l10n')
-            if anyTranslations
-                # Update CMakeLists.txt
-                CMakeEditor::append_po_install_instructions!(Dir.pwd, 'po')
-            else
-                # Remove the empty translations directory
-                Dir.delete('po')
-            end
-        end
-    end
-
+  end
 end
