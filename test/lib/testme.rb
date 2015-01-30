30cc8b5d591e4ba067e5d7f2345790fb371bc815
Harald Sitter <sitter@kde.org>
2015-01-30 12:04:16 +0100

    testme: style fixes

--- test/lib/testme.rb
+++ test/lib/testme.rb
@@ -1,27 +1,25 @@
-require "tmpdir"
-require "fileutils"
-require "test/unit"
+require 'tmpdir'
+require 'fileutils'
+require 'test/unit'
 
 class Testme < Test::Unit::TestCase
-    attr_reader :tmpdir
-    attr_reader :testdir
-    attr_reader :datadir
+  attr_reader :tmpdir
+  attr_reader :testdir
+  attr_reader :datadir
 
-    def priority_setup
-        @tmpdir = Dir.mktmpdir("testme-#{self.class.to_s}")
-        @testdir = "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}"
-        @datadir = "#{@testdir}/data"
-        Dir.chdir(@tmpdir)
-    end
+  def priority_setup
+    @tmpdir = Dir.mktmpdir("testme-#{self.class}")
+    @testdir = "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}"
+    @datadir = "#{@testdir}/data"
+    Dir.chdir(@tmpdir)
+  end
 
-    def priority_teardown
-        FileUtils.rm_rf(@tmpdir)
-    end
+  def priority_teardown
+    FileUtils.rm_rf(@tmpdir)
+  end
 
-    def data(path)
-        if path.start_with?("data/")
-            path = path.partition("data/").last
-        end
-        return "#{@datadir}/#{path}"
-    end
+  def data(path)
+    path = path.partition('data/').last if path.start_with?('data/')
+    "#{@datadir}/#{path}"
+  end
 end
