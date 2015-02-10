8fd5daaed373c36c19f40e594d5c4fd6617d36fa
Harald Sitter <sitter@kde.org>
2015-02-10 12:16:03 +0100

    vcs: style improvements

--- lib/vcs.rb
+++ lib/vcs.rb
@@ -18,27 +18,30 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
 
+# Version control system base class.
+# Doesn't do anything on its own.
 class Vcs
   # The repository URL
   attr_accessor :repository
 
   # Does a standard get operation. Obtaining repository.url into target.
-  def get(target)
-    raise "Pure virtual"
+  def get(_target)
+    fail 'Pure virtual'
   end
 
-  # Does a standard clean operation. Removing any VCS data from target (e.g. .git/.svn etc.)
-  def clean!(target)
-    raise "Pure virtual"
+  # Does a standard clean operation. Removing any VCS data from target
+  # (e.g. .git/.svn etc.)
+  def clean!(_target)
+    fail 'Pure virtual'
   end
 
   # Construct a VCS instance from a hash defining its attributes.
   # FIXME: why is this not simply an init? Oo
   def self.from_hash(hash)
-    vcs = self.new
+    vcs = new
     hash.each do |key, value|
       vcs.send("#{key}=".to_sym, value)
     end
-    return vcs
+    vcs
   end
 end
