4ea26980e25217c1de30399f6265675831715834
Harald Sitter <sitter@kde.org>
2015-02-11 14:46:30 +0100

    make sure ts_all requires with correct relativity

--- test/ts_all.rb
+++ test/ts_all.rb
@@ -6,9 +6,11 @@ unless Kernel.respond_to?(:require_relative)
   end
 end
 
-Dir.glob("#{File.dirname(__FILE__)}/test_*.rb").each do |testfile|
-    next if File.basename(testfile) == File.basename(__FILE__)
-    next if File.basename(testfile).include?("blackbox")
-    puts "Adding Test File: #{testfile}"
-    require_relative testfile
+Dir.chdir(File.dirname(__FILE__)) do
+  Dir.glob("test_*.rb").each do |testfile|
+      next if File.basename(testfile) == File.basename(__FILE__)
+      next if File.basename(testfile).include?("blackbox")
+      puts "Adding Test File: #{testfile}"
+      require_relative testfile
+  end
 end
