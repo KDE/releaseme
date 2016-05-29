35724d2691259ec07c40859242a3cc85bdbe7b52
Jonathan Riddell <jr@jriddell.org>
2016-05-29 15:50:32 +0100

    unused require

--- plasma/lib/plasma_version.rb
+++ plasma/lib/plasma_version.rb
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'ostruct'
 require 'shellwords'
 
 # Read the VERSIONS.inc file used by releaseme/plasma scripts to set various
