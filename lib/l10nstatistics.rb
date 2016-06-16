40e9146fba8fdfa1ca11abb34f371f5ce1ae7d05
Friedrich W. H. Kossebau <kossebau@kde.org>
2016-06-16 10:13:20 +0200

    Avoid po/*/messages.mo artifacts to not leak them into created tarballs
    REVIEW: 128203

--- lib/l10nstatistics.rb
+++ lib/l10nstatistics.rb
@@ -38,7 +38,7 @@ class L10nStatistics
           untranslated = 0
 
           Dir.glob('*.po').each do |file|
-            data = `LC_ALL=C LANG=C msgfmt --statistics #{file} > /dev/stdout 2>&1`
+            data = `LC_ALL=C LANG=C msgfmt --statistics #{file} -o /dev/null > /dev/stdout 2>&1`
 
             # tear the data apart and create some variables
             data.split(',').each do |x|
