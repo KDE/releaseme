9c639a1741ae16128230193d45c2fff8f9501111
Harald Sitter <sitter@kde.org>
2016-06-17 11:13:28 +0200

    style++

--- tagme.rb
+++ tagme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #--
-# Copyright (C) 2014 Harald Sitter <apachelogger@ubuntu.com>
+# Copyright (C) 2014-2016 Harald Sitter <sitter@kde.org>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -23,19 +23,19 @@ require 'optparse'
 
 options = {}
 OptionParser.new do |opts|
-    opts.banner = "Usage: targe.rb [options]"
+  opts.banner = 'Usage: targe.rb [options]'
 
-    opts.on("--version VERSION",
-            "Version.",
-            "   Versions should be kept in purely numerical format (good: x.x.x).",
-            "   Alphanumerical version should be avoided if at all possible (bad: x.x.xbeta1).") do |v|
-        options[:version] = v
-    end
+  opts.on('--version VERSION',
+          'Version.',
+          '   Versions should be kept in purely numerical format (good: x.x.x).',
+          '   Alphanumerical version should be avoided if at all possible (bad: x.x.xbeta1).') do |v|
+    options[:version] = v
+  end
 end.parse!
 
 if options[:version].nil?
-    puts "error, you need to set version"
-    exit 1
+  puts 'error, you need to set version'
+  exit 1
 end
 
 # TODO: single project tag
@@ -51,45 +51,44 @@ require_relative 'lib/project'
 require_relative 'lib/source'
 
 class TagProject
-    attr :project, true
-    attr :git_rev, true
-
+  attr_accessor :project
+  attr_accessor :git_rev
 end
 
 # FIXME: move to lib :@
-def read_release_data()
-    projects = Array.new
-    File.open('release_data', 'r') do | file |
-        file.each_line do | line |
-            parts = line.split(';')
-            next if parts.size < 3 # If we don't manage 3 parts the line is definitely crap.
-            # 0 = project
-            # 1 = branch
-            # 2 = git rev
-            project = TagProject.new
-            project.project = Project.from_xpath(parts[0])[0]
-            project.project.vcs.branch = parts[1]
-            project.git_rev = parts[2]
-            projects << project
-        end
+def read_release_data
+  projects = []
+  File.open('release_data', 'r') do |file|
+    file.each_line do |line|
+      parts = line.split(';')
+      next if parts.size < 3 # If we don't manage 3 parts the line is definitely crap.
+      # 0 = project
+      # 1 = branch
+      # 2 = git rev
+      project = TagProject.new
+      project.project = Project.from_xpath(parts[0])[0]
+      project.project.vcs.branch = parts[1]
+      project.git_rev = parts[2]
+      projects << project
     end
-    return projects
+  end
+  projects
 end
 
-tag_projects = read_release_data()
-tag_projects.each do | tag_project |
-    puts "--- #{tag_project.project.identifier} ---"
-    source = Source.new
-    source.target = "tmp-tagme"
-    source.cleanup()
-    source.get(tag_project.project.vcs, false)
+tag_projects = read_release_data
+tag_projects.each do |tag_project|
+  puts "--- #{tag_project.project.identifier} ---"
+  source = Source.new
+  source.target = 'tmp-tagme'
+  source.cleanup
+  source.get(tag_project.project.vcs, false)
 
-    Dir.chdir(source.target) do
-        puts "::git tag -s -m 'Tagging #{options[:version]}' v#{options[:version]} #{tag_project.git_rev}"
-        %x[git tag -s -m 'Tagging #{options[:version]}' v#{options[:version]} #{tag_project.git_rev}]
-        puts "::git push --tags origin"
-        %x[git push --tags origin]
-    end
+  Dir.chdir(source.target) do
+    puts "::git tag -s -m 'Tagging #{options[:version]}' v#{options[:version]} #{tag_project.git_rev}"
+    `git tag -s -m 'Tagging #{options[:version]}' v#{options[:version]} #{tag_project.git_rev}`
+    puts '::git push --tags origin'
+    `git push --tags origin`
+  end
 
-    # TODO: impl l10n and docs and what have you
+  # TODO: impl l10n and docs and what have you
 end
