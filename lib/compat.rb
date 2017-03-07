953de4e290f1feba7c307c9ead3a0f1dda424509
Harald Sitter <sitter@kde.org>
2017-03-07 11:58:40 +0100

    change the way compat works to be less ad-hoc
    mapping as classes get defined has a major problem due to how requires work
    
    proc.rb
      require 'releaseme/x'
        require './z'
      require 'y'
        require 'releaseme/z'
    
    In this line up we'd initially load X which loads Z at this point both X
    and Z are defined. We later load Y via compat files, this loads Z, BUT
    Z is already loaded so there won't be no events as the class already exists
    and thus we cannot setup compat mapping for Z because we don't even know
    Z is a thing
    
    To solve this we'll actually make the compat mapping code more readable
    and reliable by not doing an event listener but simply require the actual
    file and then iterate all consts in the ReleaseMe module. The consts
    we haven't yet define in the root scope we'll map the others we'll ignore.
    
    This means that no matter what you do whenever a compat require is used
    it will absolutely make sure everything that can be defined is defined.

--- lib/compat.rb
+++ lib/compat.rb
@@ -34,38 +34,22 @@ if File.realpath(__FILE__) == File.absolute_path($PROGRAM_NAME)
 end
 # :nocov:
 
-# Fancy event listener. We'll get notified of all class definitions in the file
-# we require and set compat class names without module.
-# NOTE: This still means that X.new will be class ReleaseMe::X rather than
-#   simply X.
-# rubocop:disable Metrics/ParameterLists
-set_trace_func proc { |event, file, _line, _id, binding, _classname|
-  # rubocop:enable
-  unless File.absolute_path(file)
-             .start_with?(File.absolute_path("#{__dir__}/releaseme/"))
-    next
-  end
-  next if event != 'class'
-  class_name = eval('name', binding)
-  next if class_name == 'ReleaseMe' # Do not gem module itself.
-  next unless class_name.include?('::') # No need forwarding toplevel classes.
-  # Should this restriction become a problem we'll need to establish a whitelist
-  # of entities we want to compat map. i.e. old classes. Fully mapping
-  # nested modules/classes is neither called for nor useful.
-  if class_name.count(':') > 2
-    warn "Not compat mapping #{class_name} as it's nested."
-    next
-  end
-  klass = Object.const_get(class_name)
-  class_name_base = class_name.split('::')[-1]
-  warn "Compat mapping #{class_name_base} ➜ #{class_name}"
-  Object.const_set(class_name_base.to_sym, klass)
-}
-
 basename = File.basename(__FILE__)
 warn <<-EOF
 Warning: requiring old file #{basename}, should require releaseme/#{basename}
   instead @ #{caller[0]}
 EOF
 require_relative "releaseme/#{basename}"
-set_trace_func(nil) # unset listener again
+
+# Compatibility map all consts out of the gem module into the root Object
+# scope.
+ReleaseMe.constants.each do |class_name|
+  next if class_name == 'ReleaseMe' # Do not map gem module itself.
+  next if Object.const_defined?(class_name)
+  # Should this restriction become a problem we'll need to establish a whitelist
+  # of entities we want to compat map. i.e. old classes. Fully mapping
+  # nested modules/classes is neither called for nor useful.
+  klass = ReleaseMe.const_get(class_name)
+  warn "Compat mapping ReleaseMe::#{class_name} ➜ #{class_name}"
+  Object.const_set(class_name, klass)
+end
