df027b19219cba86a238d5c146d5f59ff612c60f
Harald Sitter <sitter@kde.org>
2017-03-07 11:22:15 +0100

    make the require test not garbage to start porting tests
    also change the test to in fact run during class eval and define methods
    per-file such that asserts abort per-file not the entire thing

--- test/test_compat.rb
+++ test/test_compat.rb
@@ -19,7 +19,6 @@
 #++
 
 require_relative 'lib/testme'
-require_relative '../lib/archive_signer'
 
 class TestCompat < Testme
   def test_compat_compat
@@ -34,28 +33,29 @@ class TestCompat < Testme
     assert_not_nil(ReleaseMeCompatCompat.new) # able to init
   end
 
-  def test_tests garbage
-    # Make sure tests load the new files rather than the old ones.
-    lib_dir = "#{File.dirname(__dir__)}/lib/"
-    gem_dir = "#{lib_dir}/releaseme"
-    assert_path_exist(lib_dir)
-    Dir.chdir(__dir__) do
-      Dir.glob('*.rb').each do |file|
-        next if file == File.basename(__FILE__)
-        next unless file.include?('archive_sign') # we are allowed to!
-        File.read(file).each_line do |line|
-          data = line.match(/^\s*require_relative\s+["|']([^\s]+)["|'].*$/)
-          next unless data
-          require_data = data[1]
-          required_file = File.expand_path(require_data)
-          next unless required_file.start_with?(lib_dir)
-          assert(required_file.start_with?(gem_dir), <<-EOF)
+  # Make sure tests load the new files rather than the old ones.
+  lib_dir = "#{File.dirname(__dir__)}/lib/"
+  gem_dir = "#{lib_dir}/releaseme"
+  Dir.glob("#{__dir__}/*.rb").each do |file|
+    basename = File.basename(file)
+    next if basename == File.basename(__FILE__)
+    next if basename.include?('test_compat.rb') # we are allowed to!
+    sanename = basename.delete(' ').delete('/').delete('.')
+    define_method("test_#{sanename}".to_sym) do
+      # Hop into dir as otherwise we won't be able to expand_path properly.
+      Dir.chdir(__dir__)
+      File.read(file).each_line do |line|
+        data = line.match(/^\s*require_relative\s+["|']([^\s]+)["|'].*$/)
+        next unless data
+        require_data = data[1]
+        required_file = File.expand_path(require_data)
+        next unless required_file.start_with?(lib_dir)
+        assert(required_file.start_with?(gem_dir), <<-EOF)
 The tests/#{File.basename(file)} requires #{require_data} which appears to be a
 library that is in the legacy lib dir rather than the new gem dir. Tests
 must require the new files rather than the old ones.
 Require should be '../lib/releaseme/' rather than '../lib/'.
 EOF
-        end
       end
     end
   end
