282b63fa28220ee9d70999e73b744bd3bc9fad57
Harald Sitter <sitter@kde.org>
2017-03-08 11:07:52 +0100

    fixate indentation to 2spaces

--- test/test_l10n.rb
+++ test/test_l10n.rb
@@ -1,3 +1,23 @@
+#--
+# Copyright (C) 2015 Harald Sitter <sitter@kde.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy
+# defined in Section 14 of version 3 of the license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
 require 'fileutils'
 
 require_relative 'lib/testme'
@@ -8,95 +28,95 @@ require_relative '../lib/releaseme/documentation.rb'
 # FIXME: test stable
 
 class TestL10n < Testme
-    def setup
-        @repoDataDir = data("l10nrepo/")
+  def setup
+    @repoDataDir = data("l10nrepo/")
 
-        @i18n_path = "extragear-multimedia"
+    @i18n_path = "extragear-multimedia"
 
-        @trunkUrl = "trunk/l10n-kf5/"
-        @stableUrl = "branches/stable/l10n-kf5"
+    @trunkUrl = "trunk/l10n-kf5/"
+    @stableUrl = "branches/stable/l10n-kf5"
 
-        @dir = "tmp_l10n_" + (0...16).map{ ('a'..'z').to_a[rand(26)] }.join
-        @svnTemplateDir = "tmp_l10n_repo_" + (0...16).map{ ('a'..'z').to_a[rand(26)] }.join
-        @svnCheckoutDir = "tmp_l10n_check_" + (0...16).map{ ('a'..'z').to_a[rand(26)] }.join
+    @dir = "tmp_l10n_" + (0...16).map{ ('a'..'z').to_a[rand(26)] }.join
+    @svnTemplateDir = "tmp_l10n_repo_" + (0...16).map{ ('a'..'z').to_a[rand(26)] }.join
+    @svnCheckoutDir = "tmp_l10n_check_" + (0...16).map{ ('a'..'z').to_a[rand(26)] }.join
 
-        `svnadmin create #{@svnTemplateDir}`
-        assert(File.exist?(@svnTemplateDir))
+    `svnadmin create #{@svnTemplateDir}`
+    assert(File.exist?(@svnTemplateDir))
 
-        `svn co file://#{Dir.pwd}/#{@svnTemplateDir} #{@svnCheckoutDir}`
-        FileUtils.cp_r("#{@repoDataDir}/trunk", @svnCheckoutDir)
-        FileUtils.cp_r("#{@repoDataDir}/branches", @svnCheckoutDir)
-        Dir.chdir(@svnCheckoutDir) do
-          `svn add *`
-          `svn ci -m 'yolo'`
-        end
+    `svn co file://#{Dir.pwd}/#{@svnTemplateDir} #{@svnCheckoutDir}`
+    FileUtils.cp_r("#{@repoDataDir}/trunk", @svnCheckoutDir)
+    FileUtils.cp_r("#{@repoDataDir}/branches", @svnCheckoutDir)
+    Dir.chdir(@svnCheckoutDir) do
+      `svn add *`
+      `svn ci -m 'yolo'`
     end
+  end
 
-    def teardown
-        FileUtils.rm_rf(@svnTemplateDir)
-        FileUtils.rm_rf(@svnCheckoutDir)
-        FileUtils.rm_rf(@dir)
-    end
+  def teardown
+    FileUtils.rm_rf(@svnTemplateDir)
+    FileUtils.rm_rf(@svnCheckoutDir)
+    FileUtils.rm_rf(@dir)
+  end
 
-    def create_l10n
-        l = ReleaseMe::L10n.new(ReleaseMe::L10n::TRUNK, 'amarok', @i18n_path)
-        l.target = "#{@dir}/l10n"
-        l
-    end
+  def create_l10n
+    l = ReleaseMe::L10n.new(ReleaseMe::L10n::TRUNK, 'amarok', @i18n_path)
+    l.target = "#{@dir}/l10n"
+    l
+  end
 
-    def test_find_templates
-        l = create_l10n
+  def test_find_templates
+    l = create_l10n
 
-        templates = l.find_templates(data("multi-pot"))
-        assert_equal(templates.count, 2)
+    templates = l.find_templates(data("multi-pot"))
+    assert_equal(templates.count, 2)
 
-        templates = l.find_templates(data("single-pot"))
-        assert_equal(templates.count, 1)
-    end
+    templates = l.find_templates(data("single-pot"))
+    assert_equal(templates.count, 1)
+  end
 
-    def test_get_po
-        l = create_l10n
-        l.init_repo_url("file://#{Dir.pwd}/#{@svnTemplateDir}")
+  def test_get_po
+    l = create_l10n
+    l.init_repo_url("file://#{Dir.pwd}/#{@svnTemplateDir}")
 
-        FileUtils.rm_rf(@dir)
-        FileUtils.cp_r(data("single-pot"), @dir)
-        l.get(@dir)
-        assert(File.exist?("#{@dir}"))
-        assert(File.exist?("#{@dir}/CMakeLists.txt"))
-        assert(!File.exist?("#{@dir}/l10n")) # temp dir must not be there
-        assert(File.exist?("#{@dir}/po"))
-        assert(File.exist?("#{@dir}/po/de/amarok.po"))
+    FileUtils.rm_rf(@dir)
+    FileUtils.cp_r(data("single-pot"), @dir)
+    l.get(@dir)
+    assert(File.exist?("#{@dir}"))
+    assert(File.exist?("#{@dir}/CMakeLists.txt"))
+    assert(!File.exist?("#{@dir}/l10n")) # temp dir must not be there
+    assert(File.exist?("#{@dir}/po"))
+    assert(File.exist?("#{@dir}/po/de/amarok.po"))
 
-        FileUtils.rm_rf(@dir)
-        FileUtils.cp_r(data("multi-pot"), @dir)
-        l.get(@dir)
-        assert(File.exist?("#{@dir}"))
-        assert(File.exist?("#{@dir}/CMakeLists.txt"))
-        assert(!File.exist?("#{@dir}/l10n")) # temp dir must not be there
-        assert(File.exist?("#{@dir}/po"))
-        assert(File.exist?("#{@dir}/po/de/amarok.po"))
-        assert(File.exist?("#{@dir}/po/de/amarokcollectionscanner_qt.po"))
-    end
+    FileUtils.rm_rf(@dir)
+    FileUtils.cp_r(data("multi-pot"), @dir)
+    l.get(@dir)
+    assert(File.exist?("#{@dir}"))
+    assert(File.exist?("#{@dir}/CMakeLists.txt"))
+    assert(!File.exist?("#{@dir}/l10n")) # temp dir must not be there
+    assert(File.exist?("#{@dir}/po"))
+    assert(File.exist?("#{@dir}/po/de/amarok.po"))
+    assert(File.exist?("#{@dir}/po/de/amarokcollectionscanner_qt.po"))
+  end
 
-    def test_statistics
-        l = create_l10n
-        l.init_repo_url("file://#{Dir.pwd}/#{@svnTemplateDir}")
+  def test_statistics
+    l = create_l10n
+    l.init_repo_url("file://#{Dir.pwd}/#{@svnTemplateDir}")
 
-        FileUtils.rm_rf(@dir)
-        FileUtils.cp_r(data("multi-pot"), @dir)
-        l.get(@dir)
+    FileUtils.rm_rf(@dir)
+    FileUtils.cp_r(data("multi-pot"), @dir)
+    l.get(@dir)
 
-        statistics = ReleaseMe::L10nStatistics.new
-        statistics.gather!(@dir)
-        assert_equal(statistics.stats, {"de"=>{:all=>4,
-                                           :shown=>3,
-                                           :notshown=>1,
-                                           :percentage=>75.0},
-                                        "fr"=>{:all=>4,
-                                           :shown=>4,
-                                           :notshown=>0,
-                                           :percentage=>100.0}})
-    end
+    statistics = ReleaseMe::L10nStatistics.new
+    statistics.gather!(@dir)
+    assert_equal(statistics.stats, {"de"=>{:all=>4,
+                                       :shown=>3,
+                                       :notshown=>1,
+                                       :percentage=>75.0},
+                                    "fr"=>{:all=>4,
+                                       :shown=>4,
+                                       :notshown=>0,
+                                       :percentage=>100.0}})
+  end
 
   def test_variable_potname
     l = create_l10n
