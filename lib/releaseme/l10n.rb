7be74a1b40094852f7f23354edd1e60ee7c3348f
Harald Sitter <sitter@kde.org>
2017-04-21 12:11:03 +0200

    take apart the l10n.get() pasta

--- lib/releaseme/l10n.rb
+++ lib/releaseme/l10n.rb
@@ -51,156 +51,10 @@ module ReleaseMe
       languages_without_translation = []
       # FIXME: due to threading we do explicit pathing, so this probably can go
       Dir.chdir(srcdir) do
-        queue = languages_queue
-        threads = []
-        script_cache = L10nScriptDownloader::TemplateCache.new(self)
-        THREAD_COUNT.times do
-          threads << Thread.new do
-            Thread.current.abort_on_exception = true
-            until queue.empty?
-              begin
-                lang = queue.pop(true)
-              rescue
-                # When pop runs into an empty queue with non_block=true it raises
-                # an exception. We'll simply continue with it as our loop should
-                # naturally end anyway.
-                continue
-              end
-              Dir.mktmpdir(self.class.to_s) do |tmpdir|
-                log_debug "#{srcdir} - downloading #{lang}"
-                files = []
-
-                # Data assets are not linked to a template, so we can run these
-                # before even looking at the templates in detail.
-                files += L10nDataDownloader.new(lang, tmpdir, self).download
-
-                if templates.count > 1
-                  files += get_multiple(lang, tmpdir)
-                elsif templates.count == 1
-                  files += get_single(lang, tmpdir)
-                end
-                # No translations need fetching. But continue because not
-                # all assets are template bound.
-
-                files += L10nScriptDownloader.new(lang, tmpdir, script_cache,
-                                                  self).download
-
-                files = files.compact.uniq
-
-                # No files obtained :(
-                if files.empty?
-                  # FIXME: not thread safe without GIL
-                  languages_without_translation << lang
-                  next
-                end
-
-                # TODO: path confusing with target
-                files.each do |file|
-                  file = L10nAsset.new(file)
-                  file.strip!
-                  destination = if file.qt? && !kde4_origin?
-                                  "#{qttarget}/#{lang}"
-                                else
-                                  "#{target}/#{lang}"
-                                end
-                  FileUtils.mkpath(destination)
-                  FileUtils.mv(file, destination)
-                end
-              end
-
-              # FIXME: this is not thread safe without a GIL
-              @languages += [lang]
-            end
-          end
-        end
-        ThreadsWait.all_waits(threads)
-
-        if ENV.include?('RELEASEME_L10N_REQUIREMENT')
-          completion_requirement = ENV['RELEASEME_L10N_REQUIREMENT'].to_i
-          require_relative 'l10nstatistics'
-          [target, qttarget].each do |dir|
-            stats = L10nStatistics.new.tap { |l| l.gather!(dir) }.stats
-            stats.each do |lang, stat|
-              next if stat[:percentage] >= completion_requirement
-              FileUtils.rm_r("#{dir}/#{lang}", verbose: true)
-            end
-          end
-        end
-
-        # Update Data after mangling
-
-        po_files = Dir.glob("#{target}/**/**")
-        po_files.select! { |x| File.file?(x) }
-
-        qt_files = Dir.glob("#{qttarget}/**/**")
-        qt_files.select! { |x| File.file?(x) }
-
-        has_po_translations = !po_files.empty?
-        has_qt_translations = !qt_files.empty?
-
-        if has_po_translations
-          if edit_cmake
-            # Update master CMakeLists.txt
-            # FIXME: Dir.pwd because we chdir above and never undo that.
-            CMakeEditor.append_po_install_instructions!(Dir.pwd, 'po')
-          end
-
-          # Create po's CMakeLists.txt if there are data assets we need to
-          # install. Data assets rely on CMakeLists.txt supplied by
-          # translators, we still need to assemble the directories with assets
-          # into the po/CMakeLists.txt though.
-          data_assets = Dir.glob("#{target}/*/data/*")
-          unless data_assets.empty?
-            File.open("#{target}/CMakeLists.txt", 'a') do |f|
-              data_assets.each do |dir|
-                f.puts(CMakeEditor.add_subdirectory(dir, relative_to: target))
-              end
-            end
-            if edit_cmake
-              # FIXME: Dir.pwd because we chdir above and never undo that.
-              CMakeEditor.append_optional_add_subdirectory!(Dir.pwd, 'po')
-            end
-          end
-
-          # cmake_modules may be used by data assets, they are meant to be
-          # folded into a join cmake_modules dir (doesn't make sense to me
-          # why though, seems to me that does nothing but cause risk of clash)
-          mod_target = "#{target}/cmake_modules".freeze
-          Dir.glob("#{target}/*/cmake_modules").each do |mod|
-            content = Dir.glob("#{mod}/*").reject { |x| x.include?('.svn') }
-            FileUtils.mkpath(mod_target)
-            FileUtils.cp_r(content, mod_target, verbose: true)
-            FileUtils.rm_r(mod)
-          end
-        end
-
-        # Process Qt translations. This MUST be after the PO translations as
-        # we may wish to reuse the PO location if there's only Qt translations.
-        if has_qt_translations
-          final_target = qttarget # Can get modified.
-
-          unless has_po_translations
-            log_info 'Found Qt translations, but no Gettext translations.' \
-                     ' Storing Qt translations in po/ dir.'
-            FileUtils.rm_r(target)
-            FileUtils.mv(qttarget, target)
-            # Prevent us from getting deleted in the new target.
-            has_po_translations = has_qt_translations
-            final_target = target
-          end
-
-          if edit_cmake
-            # Update master CMakeLists.txt
-            # FIXME: handle *name in CMakeEditor, this is way too long a line.
-            CMakeEditor
-              .append_poqm_install_instructions!(File.dirname(final_target),
-                                                 File.basename(final_target))
-          end
-        end
-
-        # Remove the empty translations directory
-        Dir.delete(target) unless has_po_translations
-        Dir.delete(qttarget) unless has_qt_translations
+        # FIXME: languages_without_translation is super naughty here. it is
+        #   filled by reference inside threads... super shit not thread safe
+        download(srcdir, languages_without_translation, target, qttarget)
+        post_process(target, qttarget, edit_cmake)
       end
 
       return if languages_without_translation.empty?
@@ -208,6 +62,161 @@ module ReleaseMe
     end
 
     private
+
+    def download(srcdir, languages_without_translation, target, qttarget)
+      queue = languages_queue
+      threads = []
+      script_cache = L10nScriptDownloader::TemplateCache.new(self)
+      THREAD_COUNT.times do
+        threads << Thread.new do
+          Thread.current.abort_on_exception = true
+          until queue.empty?
+            begin
+              lang = queue.pop(true)
+            rescue
+              # When pop runs into an empty queue with non_block=true it raises
+              # an exception. We'll simply continue with it as our loop should
+              # naturally end anyway.
+              continue
+            end
+            Dir.mktmpdir(self.class.to_s) do |tmpdir|
+              log_debug "#{srcdir} - downloading #{lang}"
+              files = []
+
+              # Data assets are not linked to a template, so we can run these
+              # before even looking at the templates in detail.
+              files += L10nDataDownloader.new(lang, tmpdir, self).download
+
+              if templates.count > 1
+                files += get_multiple(lang, tmpdir)
+              elsif templates.count == 1
+                files += get_single(lang, tmpdir)
+              end
+              # No translations need fetching. But continue because not
+              # all assets are template bound.
+
+              files += L10nScriptDownloader.new(lang, tmpdir, script_cache,
+                                                self).download
+
+              files = files.compact.uniq
+
+              # No files obtained :(
+              if files.empty?
+                # FIXME: not thread safe without GIL
+                languages_without_translation << lang
+                next
+              end
+
+              # TODO: path confusing with target
+              files.each do |file|
+                file = L10nAsset.new(file)
+                file.strip!
+                destination = if file.qt? && !kde4_origin?
+                                "#{qttarget}/#{lang}"
+                              else
+                                "#{target}/#{lang}"
+                              end
+                FileUtils.mkpath(destination)
+                FileUtils.mv(file, destination)
+              end
+            end
+
+            # FIXME: this is not thread safe without a GIL
+            @languages += [lang]
+          end
+        end
+      end
+      ThreadsWait.all_waits(threads)
+    end
+
+    def post_process(target, qttarget, edit_cmake)
+      if ENV.include?('RELEASEME_L10N_REQUIREMENT')
+        completion_requirement = ENV['RELEASEME_L10N_REQUIREMENT'].to_i
+        require_relative 'l10nstatistics'
+        [target, qttarget].each do |dir|
+          stats = L10nStatistics.new.tap { |l| l.gather!(dir) }.stats
+          stats.each do |lang, stat|
+            next if stat[:percentage] >= completion_requirement
+            FileUtils.rm_r("#{dir}/#{lang}", verbose: true)
+          end
+        end
+      end
+
+      # Update Data after mangling
+
+      po_files = Dir.glob("#{target}/**/**")
+      po_files.select! { |x| File.file?(x) }
+
+      qt_files = Dir.glob("#{qttarget}/**/**")
+      qt_files.select! { |x| File.file?(x) }
+
+      has_po_translations = !po_files.empty?
+      has_qt_translations = !qt_files.empty?
+
+      if has_po_translations
+        if edit_cmake
+          # Update master CMakeLists.txt
+          # FIXME: Dir.pwd because we chdir above and never undo that.
+          CMakeEditor.append_po_install_instructions!(Dir.pwd, 'po')
+        end
+
+        # Create po's CMakeLists.txt if there are data assets we need to
+        # install. Data assets rely on CMakeLists.txt supplied by
+        # translators, we still need to assemble the directories with assets
+        # into the po/CMakeLists.txt though.
+        data_assets = Dir.glob("#{target}/*/data/*")
+        unless data_assets.empty?
+          File.open("#{target}/CMakeLists.txt", 'a') do |f|
+            data_assets.each do |dir|
+              f.puts(CMakeEditor.add_subdirectory(dir, relative_to: target))
+            end
+          end
+          if edit_cmake
+            # FIXME: Dir.pwd because we chdir above and never undo that.
+            CMakeEditor.append_optional_add_subdirectory!(Dir.pwd, 'po')
+          end
+        end
+
+        # cmake_modules may be used by data assets, they are meant to be
+        # folded into a join cmake_modules dir (doesn't make sense to me
+        # why though, seems to me that does nothing but cause risk of clash)
+        mod_target = "#{target}/cmake_modules".freeze
+        Dir.glob("#{target}/*/cmake_modules").each do |mod|
+          content = Dir.glob("#{mod}/*").reject { |x| x.include?('.svn') }
+          FileUtils.mkpath(mod_target)
+          FileUtils.cp_r(content, mod_target, verbose: true)
+          FileUtils.rm_r(mod)
+        end
+      end
+
+      # Process Qt translations. This MUST be after the PO translations as
+      # we may wish to reuse the PO location if there's only Qt translations.
+      if has_qt_translations
+        final_target = qttarget # Can get modified.
+
+        unless has_po_translations
+          log_info 'Found Qt translations, but no Gettext translations.' \
+                   ' Storing Qt translations in po/ dir.'
+          FileUtils.rm_r(target)
+          FileUtils.mv(qttarget, target)
+          # Prevent us from getting deleted in the new target.
+          has_po_translations = has_qt_translations
+          final_target = target
+        end
+
+        if edit_cmake
+          # Update master CMakeLists.txt
+          # FIXME: handle *name in CMakeEditor, this is way too long a line.
+          CMakeEditor
+            .append_poqm_install_instructions!(File.dirname(final_target),
+                                               File.basename(final_target))
+        end
+      end
+
+      # Remove the empty translations directory
+      Dir.delete(target) unless has_po_translations
+      Dir.delete(qttarget) unless has_qt_translations
+    end
 
     def verify_pot(potname)
       return unless potname.include?('$')
