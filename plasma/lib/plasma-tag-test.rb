740abb6e7cce5b164299498c095e84ee7db6232b
Jonathan Riddell <jr@jriddell.org>
2017-10-17 15:50:20 +0100

    rubocop

--- plasma/lib/plasma-tag-test.rb
+++ plasma/lib/plasma-tag-test.rb
@@ -3,45 +3,40 @@
 require_relative 'plasma_version'
 require 'git'
 
+# check the tag has been pushed
 class PlasmaTagTest
-    attr_accessor :version
-    attr_accessor :repos
+  attr_accessor :version
+  attr_accessor :repos
 
-    def initialize()
-      plasma_versions = PlasmaVersion.new
-      @version = plasma_versions.version
-      Dir.chdir(plasma_versions.plasma_clones)
-      @version = "5.11.0"
-    end
+  def initialize
+    plasma_versions = PlasmaVersion.new
+    @version = plasma_versions.version
+    Dir.chdir(plasma_versions.plasma_clones)
+    @version = '5.11.0'
+  end
 
-    def get_git_repos()
-      fileContents = File.read('/home/jr/src/releaseme/releaseme/plasma/git-repositories-for-release')
-      @repos = fileContents.split(' ')
-      discover = @repos.find_index('discover')
-      @repos[discover] = 'plasma-discover'
-    end
+  def grab_git_repos
+    file_contents = File.read( \
+      '/home/jr/src/releaseme/releaseme/plasma/git-repositories-for-release' \
+    )
+    @repos = file_contents.split(' ')
+    discover = @repos.find_index('discover')
+    @repos[discover] = 'plasma-discover'
+  end
 
-    def check_tags
-      repos.each do |repo|
-        Dir.chdir(repo + '/kdegit/' + repo) do
-          found = false
-          system('git fetch --tags')
-          git = Git.open('.')
-          git.tags.each do |tag|
-            if tag.name == "v#{@version}"
-              found = true
-            end
-          end
-          puts "Not found #{repo}" if found == false
-          exit 0 if found == false
+  def check_tags
+    repos.each do |repo|
+      Dir.chdir(repo + '/kdegit/' + repo) do
+        found = false
+        system('git fetch --tags')
+        git = Git.open('.')
+        git.tags.each do |tag|
+          found = true if tag.name == "v#{@version}"
         end
+        puts "Not found #{repo}" if found == false
+        exit 0 if found == false
       end
-      puts "All good!"
     end
+    puts 'All good!'
+  end
 end
-
-#class with methods to:
-# get list of git repos
-# for each repo git tag
-# check it has tag for version
-# print result
