31579ac9c3f0b96a7738ff5f88308b68adcd303f
Jonathan Riddell <jr@jriddell.org>
2018-09-13 19:21:49 +0100

    plasma 5.14 beta

--- plasma/lib/plasma-tag-test.rb
+++ plasma/lib/plasma-tag-test.rb
@@ -12,7 +12,7 @@ class PlasmaTagTest
     plasma_versions = PlasmaVersion.new
     @version = plasma_versions.version
     Dir.chdir(plasma_versions.plasma_clones)
-    @version = '5.13.4'
+    @version = '5.13.90'
   end
 
   def grab_git_repos
