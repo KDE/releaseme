f52af756ff7128bfc5a8b19cdf7799be7969e05e
Harald Sitter <sitter@kde.org>
2021-02-24 16:06:32 +0100

    license more stuff
    as discussed on the plasma-devel mailing list

--- test/plasma-webpages_test.rb
+++ test/plasma-webpages_test.rb
@@ -1,20 +1,8 @@
-# Copyright (C) 2016 Jonathan Riddell <jr@jriddell.org>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License or (at your option) version 3 or any later version
-# accepted by the membership of KDE e.V. (or its successor approved
-# by the membership of KDE e.V.), which shall act as a proxy
-# defined in Section 14 of version 3 of the license.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+# SPDX-FileCopyrightText: 2016-2020 Jonathan Riddell <jr@jriddell.org>
+# SPDX-FileCopyrightText: 2016-2017 Harald Sitter <sitter@kde.org>
+# SPDX-FileCopyrightText: 2018 Bhushan Shah <bhush94@gmail.com>
+# SPDX-FileCopyrightText: 2020 Carl Schwan <carl@carlschwan.eu>
 
 require 'fileutils'
 
--- test/translationunit_test.rb
+++ test/translationunit_test.rb
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+# SPDX-FileCopyrightText: 2015-2020 Harald Sitter <sitter@kde.org>
+# SPDX-FileCopyrightText: 2017 Jonathan Riddell <jr@jriddell.org>
+
 require_relative 'lib/testme'
 require_relative '../lib/releaseme/translationunit'
 
