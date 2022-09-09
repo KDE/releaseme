f3fb64566a04ea440314347b6460e24cfdeaf094
Aleix Pol <aleixpol@kde.org>
2022-09-09 15:38:41 +0200

    Fill in the form values as necessary
    I'd always filled them in as done here by hand, I guess there's not much
    use for it.

--- lib/releaseme/release.rb
+++ lib/releaseme/release.rb
@@ -128,7 +128,7 @@ module ReleaseMe
       title = "Publish #{tar}"
       sha256s = [sig, tar].collect { |x| `sha256sum #{x}`.strip }
       sha1s = [sig, tar].collect { |x| `sha1sum #{x}`.strip }
-      template = HashTemplate.new(sha256s: sha256s, sha1s: sha1s, version: version)
+      template = HashTemplate.new(sha256s: sha256s, sha1s: sha1s, version: version, projectName: project.identifier, origin: origin)
       template_file = "#{__dir__}/data/ticket_description.txt.erb"
       description = template.render(template_file)
       sysadmin_ticket_uri(title: title, description: description)
