205ca3b74103eadd0f19304fa473a5f450f51293
Jonathan Esk-Riddell <jr@jriddell.org>
2022-11-09 11:39:41 +0000

    reuse happyness

--- plasma/lib/kde_identify.rb
+++ plasma/lib/kde_identify.rb
@@ -1,4 +1,6 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+# SPDX-FileCopyrightText: 2023 Jonathan Esk-Riddell <jr@jriddell.org>
 
 require 'httparty'
 
