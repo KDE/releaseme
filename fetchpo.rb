cd2b6de9ef2e2c73d0fecf565132d6c5f084eb61
Jonathan Esk-Riddell <jr@jriddell.org>
2023-04-13 13:17:21 +0100

    reuse happyness

--- fetchpo.rb
+++ fetchpo.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+# SPDX-FileCopyrightText: 2023 Jonathan Esk-Riddell <jr@jriddell.org>
+
 # Translation support has been removed from releaseme in favour of scripty
 # Retain this script as a dummy due to use in extra-make-modules
 
