b3e15d0cfe3d7f3ee4e0dff9dfdee5544dff85d6
Harald Sitter <sitter@kde.org>
2023-06-05 14:15:08 +0200

    bump compatibility to ruby 3.2
    we aren't affected by any of the depreciation removals

--- test/requirement_checker_test.rb
+++ test/requirement_checker_test.rb
@@ -71,6 +71,7 @@ class TestRequirementChecker < Testme
       2.7.0
       3.0.0
       3.1.0
+      3.2.0
     ]
     compatibles.each { |i| assert_ruby_version_compatible(i) }
     incompatibles = %w[
@@ -88,8 +89,8 @@ class TestRequirementChecker < Testme
       2.5.1
       2.8
       2.8.1
-      3.2
-      3.2.0
+      3.3
+      3.3.0
     ]
     incompatibles.each { |i| assert_ruby_version_not_compatible(i) }
   end
