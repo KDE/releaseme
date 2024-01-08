b09d2bcb346c74399db9aaab7b68d843d1364077
Harald Sitter <sitter@kde.org>
2024-01-08 10:29:24 +0100

    bump compatibility to ruby 3.3
    no particularly relevant changes for us

--- test/requirement_checker_test.rb
+++ test/requirement_checker_test.rb
@@ -72,6 +72,7 @@ class TestRequirementChecker < Testme
       3.0.0
       3.1.0
       3.2.0
+      3.3.0
     ]
     compatibles.each { |i| assert_ruby_version_compatible(i) }
     incompatibles = %w[
@@ -89,8 +90,8 @@ class TestRequirementChecker < Testme
       2.5.1
       2.8
       2.8.1
-      3.3
-      3.3.0
+      3.4
+      3.4.0
     ]
     incompatibles.each { |i| assert_ruby_version_not_compatible(i) }
   end
